21a60be7260fed3892ecd928f1e00416e82944c5
Shristi Chand <shristichand658@gmail.com>
2025-10-09 07:40:24 +0545

    Feature Section COmplete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection/>
+        <FeatureSection/>
       </div>
     </>
   )
